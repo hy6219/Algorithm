d49363df90d025bc90df2b5005c22ca2348b59f5
JSJeong <39812154+hy6219@users.noreply.github.com>
2020-11-07 09:15:50 +0900

    Create RecursionHanoi.cpp

---
+++ HanoiTower/RecursionHanoi.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+
+void Hanoi(int num, char from, char temp, char dest)
+{
+	if (num < 1)
+	{
+		return;
+	}
+	Hanoi(num - 1, from, temp, dest);//n-1개 원반을 from에서 temp로 이동시키기
+	printf("\n move disc %d from group %c to group %c\n",num,from,dest);
+	Hanoi(num - 1, temp, dest, from);//n-1개 원반을 temp에서 dest로 이동시키기
+}
+
+
+int main()
+{
+	int num = 5;
+	printf("하노이의 탑 알고리즘 확인\n");
+	Hanoi(5, 'A', 'B', 'C');
+
+}
